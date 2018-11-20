c4a8ea991f18f53e8daf965731c0691aef68cbe4
Tobias He <xtobiashe@gmail.com>
2018-11-20 14:57:19 -0500

    added password strength indicator (#77)
    * added password strength indicator
    
    * fixed color orientation for password strength so it makes more sense

--- website/src/views/Signup/Signup.jsx
+++ website/src/views/Signup/Signup.jsx
@@ -1,5 +1,5 @@
 import {
-  Button, Card, Icon, Input, Select, Radio, message,
+  Button, Card, Icon, Input, Select, Radio, message, Progress, Alert,
 } from 'antd';
 import React, { PureComponent } from 'react';
 import firebase from 'firebase/app';
@@ -15,16 +15,18 @@ export default class Signup extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      email: null,
-      name: null,
-      password: null,
-      type: null,
-      universities: null,
+      email: '',
+      name: '',
+      password: '',
+      type: '',
+      universities: '',
       loading: false,
       collegeOptions: [],
+      strengthColor: 'red',
     };
     this.signup = this.signup.bind(this);
     this.getColleges = this.getColleges.bind(this);
+    this.passwordInputHandler = this.passwordInputHandler.bind(this);
   }
 
   componentDidMount() {
@@ -51,7 +53,7 @@ export default class Signup extends PureComponent {
     this.setState({ loading: true });
     const { history } = this.props;
     const {
-      universities, name, password, type, email,
+      universities, name, password, type, email, strengthColor,
     } = this.state;
     const reqData = {
       universities,
@@ -61,12 +63,22 @@ export default class Signup extends PureComponent {
       email,
     };
     if (!universities || !name || !password || !type || !email) {
-      message.error('Looks like you\'re missing something.');
+      message.error("Looks like you're missing something.");
       this.setState({ loading: false });
       return;
     }
     if (universities === [] || name === '' || password === '' || type === '' || email === '') {
-      message.error('Looks like you\'re missing something.');
+      message.error("Looks like you're missing something.");
+      this.setState({ loading: false });
+      return;
+    }
+    if (password.length < 6) {
+      message.error('Password must be at least 6 characters long.');
+      this.setState({ loading: false });
+      return;
+    }
+    if (strengthColor !== 'green') {
+      message.error('Password not strong enough');
       this.setState({ loading: false });
       return;
     }
@@ -77,33 +89,52 @@ export default class Signup extends PureComponent {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(reqData),
-    }).then((result) => {
-      if (result.status === 200) {
-        firebase.auth()
-          .signInWithEmailAndPassword(email, password)
-          .then((user) => {
-            if (user) {
-              message.success('Signed Up. Redirecting..');
-              if (type === 'student') {
-                history.push('/join/class');
-              } if (type === 'teacher') {
-                history.push('/create/class');
+    })
+      .then((result) => {
+        if (result.status === 200) {
+          firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((user) => {
+              if (user) {
+                message.success('Signed Up. Redirecting..');
+                if (type === 'student') {
+                  history.push('/join/class');
+                }
+                if (type === 'teacher') {
+                  history.push('/create/class');
+                }
               }
-            }
-          });
-      } else {
+            });
+        } else {
+          this.setState({ loading: false });
+          message.error(result.message ? result.message : 'Something went wrong.');
+        }
+      })
+      .catch((err) => {
+        console.log('Signup Err', err);
         this.setState({ loading: false });
-        message.error(result.message);
-      }
-    }).catch((err) => {
-      console.log('Signup Err', err);
-      this.setState({ loading: false });
-    });
+      });
+  }
+
+  passwordInputHandler(e) {
+    const hasNumber = value => new RegExp(/[0-9]/).test(value);
+    const hasMixed = value => new RegExp(/[a-z]/).test(value) && new RegExp(/[A-Z]/).test(value);
+    const hasSpecial = value => new RegExp(/[!#@$%^&*)(+=._-]/).test(value);
+    let strength = 0;
+    if (hasNumber(e.target.value)) strength += 1;
+    if (hasSpecial(e.target.value)) strength += 1;
+    if (hasMixed(e.target.value)) strength += 1;
+    if (strength === 0) this.setState({ strengthColor: 'red', password: e.target.value });
+    if (strength === 1) this.setState({ strengthColor: 'orange', password: e.target.value });
+    if (strength === 2) this.setState({ strengthColor: 'yellow', password: e.target.value });
+    if (e.target.value.length >= 6 && strength === 3) this.setState({ strengthColor: 'green', password: e.target.value });
+    else if (e.target.value.length < 6 && strength === 3) this.setState({ strengthColor: 'lightgreen', password: e.target.value });
   }
 
   render() {
     const {
-      type, name, loading, collegeOptions,
+      type, name, loading, collegeOptions, password, strengthColor,
     } = this.state;
     const { history } = this.props;
     const user = firebase.auth().currentUser;
@@ -113,16 +144,14 @@ export default class Signup extends PureComponent {
     return (
       <div className="signup">
         <h1 className="title">Rails</h1>
-        <h5 className="tag-line">Keep students on track</h5>
-        <Card
-          className="signup-card"
-          title="Sign Up"
-        >
+        <Card className="signup-card" title="Sign Up">
           <div>
             <Radio.Group
               value={type}
               buttonStyle="solid"
-              onChange={(e) => { this.setState({ type: e.target.value }); }}
+              onChange={(e) => {
+                this.setState({ type: e.target.value });
+              }}
               className="user-type-radio"
             >
               <Radio.Button value="student">Student</Radio.Button>
@@ -137,14 +166,9 @@ export default class Signup extends PureComponent {
               style={{ width: '100%' }}
               onChange={e => this.setState({ universities: e })}
             >
-              {
-                    collegeOptions.map(element => (
-                      <Select.Option key={element['school.name']}>
-                        {element['school.name']}
-                      </Select.Option>
-                    ))
-
-                }
+              {collegeOptions.map(element => (
+                <Select.Option key={element['school.name']}>{element['school.name']}</Select.Option>
+              ))}
             </Select>
             <Input
               size="large"
@@ -152,7 +176,9 @@ export default class Signup extends PureComponent {
               prefix={<Icon type="user" />}
               style={{ marginTop: 10 }}
               value={name}
-              onChange={(e) => { this.setState({ name: e.target.value }); }}
+              onChange={(e) => {
+                this.setState({ name: e.target.value });
+              }}
             />
             <Input
               size="large"
@@ -160,7 +186,9 @@ export default class Signup extends PureComponent {
               prefix={<Icon type="mail" />}
               style={{ marginTop: 10 }}
               type="email"
-              onChange={(e) => { this.setState({ email: e.target.value }); }}
+              onChange={(e) => {
+                this.setState({ email: e.target.value });
+              }}
             />
             <Input
               size="large"
@@ -168,9 +196,29 @@ export default class Signup extends PureComponent {
               prefix={<Icon type="ellipsis" />}
               style={{ marginTop: 10 }}
               type="password"
-              onChange={(e) => { this.setState({ password: e.target.value }); }}
+              onChange={this.passwordInputHandler}
             />
-            <Button icon="check-circle" size="large" block style={{ marginTop: 10 }} onClick={this.signup} loading={loading} type="primary">
+
+            <Progress
+              format={(p, sp) => `${Math.round((p / 100) * 6)}/6`}
+              percent={100 * (password.length / 6)}
+              strokeColor={strengthColor}
+            />
+            {strengthColor !== 'green' ? (
+              <Alert
+                message="Password must be 6 or more characters and contain: Uppercase letter (A-Z), Lowercase letter (a-z), Digit (0-9), Special character (~`!@#$%^&*()+=_-{}[]\ "
+                type="error"
+              />
+            ) : null}
+            <Button
+              icon="check-circle"
+              size="large"
+              block
+              style={{ marginTop: 10 }}
+              onClick={this.signup}
+              loading={loading}
+              type="primary"
+            >
               <p style={{ margin: 0, display: 'inline', marginLeft: 10 }}>Sign Up</p>
             </Button>
             <Button size="large" block style={{ marginTop: 10 }} href="/signin">
