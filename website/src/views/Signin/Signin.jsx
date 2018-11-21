3af631b69870e6d5c193ee75d9facc4cc710db7b
Huang Weicheng <whuang0079@gmail.com>
2018-11-21 10:13:27 -0800

    add forgot passwrd feature. Add password and email validation function (#81)

--- website/src/views/Signin/Signin.jsx
+++ website/src/views/Signin/Signin.jsx
@@ -1,5 +1,5 @@
 import {
-  Button, Card, Icon, Input, message,
+  Button, Card, Icon, Input, message, Modal,
 } from 'antd';
 import React, { PureComponent } from 'react';
 import firebase from 'firebase/app';
@@ -17,20 +17,35 @@ export default class Signin extends PureComponent {
       email: '',
       password: '',
       loading: false,
+      showModal: false,
     };
     this.signin = this.signin.bind(this);
+    this.forgotPassword = this.forgotPassword.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  /* Email validation */
+  validateEmail (email) {
+    const filter = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (filter.test(email) && email !== '') return true;
+      return false;
+  }
+
+  /* Password validation */
+  validatePassword (password) {
+    if (password !== '' ) return true;
+      return false;
   }
 
   signin() {
     const { email, password } = this.state;
     const { location, history } = this.props;
-    if (email === '' || password === '') {
-      message.error("Looks like you're missing something.");
+    if ( !this.validateEmail(email) || !this.validatePassword(password) ) {
+      message.error('Please enter valid eamil and password');
       return;
     }
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
+    firebase.auth().signInWithEmailAndPassword(email, password)
       .then((user) => {
         if (user) {
           if (location.state !== undefined) {
@@ -45,8 +60,39 @@ export default class Signin extends PureComponent {
       });
   }
 
+/* Pop out a forgot password window */
+  forgotPassword = () => {
+    this.setState({
+      showModal: true,
+    });
+  }
+
+/* Handle cancel button in the forgot password window */
+  handleCancel = () => {
+    this.setState({
+      showModal: false,
+    });
+  }
+
+/* Handle ok button in the forgot password window, send user a password reset email using firebase function */
+  handleOk() {
+    const { email } =  this.state;
+    if ( !this.validateEmail(email) ) {
+      message.error('Please enter valid eamil');
+      return;
+    }
+    firebase.auth().sendPasswordResetEmail(email).then(function() {
+      message.success('Password reset email has been sent.')
+    }).catch(function(err) {
+      message.error(err.message);
+    });
+    this.setState({
+      showModal: false,
+    });
+}
+ 
   render() {
-    const { email, password, loading } = this.state;
+    const { email, password, loading, showModal } = this.state;
     const { history } = this.props;
     const user = firebase.auth().currentUser;
     if (user) {
@@ -56,7 +102,10 @@ export default class Signin extends PureComponent {
       <div className="signin">
         <h1 className="title">Rails</h1>
         <h5 className="tag-line">Keep students on track</h5>
-        <Card className="signin-card" title="Sign In">
+        <Card
+          className="signin-card"
+          title="Sign In"
+        >
           <div>
             <Input
               required
@@ -66,9 +115,7 @@ export default class Signin extends PureComponent {
               style={{ marginTop: 10 }}
               type="email"
               value={email}
-              onChange={(e) => {
-                this.setState({ email: e.target.value });
-              }}
+              onChange={(e) => { this.setState({ email: e.target.value }); }}
             />
             <Input
               required
@@ -78,25 +125,37 @@ export default class Signin extends PureComponent {
               style={{ marginTop: 10 }}
               type="password"
               value={password}
-              onChange={(e) => {
-                this.setState({ password: e.target.value });
-              }}
+              onChange={(e) => { this.setState({ password: e.target.value }); }}
             />
-
-            <Button
-              icon="check-circle"
-              size="large"
-              block
-              style={{ marginTop: 10 }}
-              onClick={this.signin}
-              loading={loading}
-              type="primary"
-            >
+            <Button icon="check-circle" size="large" block style={{ marginTop: 10 }} onClick={this.signin} loading={loading} type="primary">
               <p style={{ margin: 0, display: 'inline', marginLeft: 10 }}>Sign In</p>
             </Button>
             <Button size="large" block style={{ marginTop: 10 }} href="/signup">
               <p style={{ margin: 0, display: 'inline' }}>Don&#39;t have an account? Sign Up.</p>
             </Button>
+            <Button size="large" block style={{ marginTop: 10 }} onClick={this.forgotPassword}>
+              <p style={{ margin: 0, display: 'inline' }}>Forgot Password?</p>
+            </Button>
+            <Modal
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+              className="forgot-password-modal"
+              centered
+              visible={showModal}
+              title="Forgot Password"
+            >
+              <p className="forgot-password-label">Please enter your E-mail address, we will send you a password reset email:</p>
+              <Input
+                required
+                size="large"
+                placeholder="E-Mail"
+                prefix={<Icon type="mail" />}
+                style={{ marginTop: 10 }}
+                type="email"
+                value={email}
+                onChange={(e) => { this.setState({ email: e.target.value }); }}
+              />
+            </Modal>
           </div>
         </Card>
       </div>
