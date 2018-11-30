5e2bc669e900bf1bd12100c8a8231d25b6948b2e
whuang001 <vincentwongad@gmail.com>
2018-11-30 15:29:20 -0500

    remove snapshot test for Notification, use enzyme testing instead

--- website/src/components/Notification/Notification.test.jsx
+++ website/src/components/Notification/Notification.test.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { Drawer, Card } from 'antd';
+import {
+  shallow,
+  configure,
+} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { Notification } from '..';
 
+configure({ adapter: new Adapter() });
+
 describe('Notification', () => {
-  it('Render Notification without any eror', () => {
-    expect(renderer.create(<Notification />).toJSON()).toMatchSnapshot();
+  /* enzyme testing */
+  const wrapper = shallow(<Notification notificationVisible={true} onClose={() => {}} />);
+
+  it('Drawer should display correctly', () => {
+    expect(wrapper.type()).toBe(Drawer);
   });
 });
