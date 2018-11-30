43a69e9d3c0e258b404eecc0bb6320c2dfaddb64
edwin0108 <qmpyv2000@hotmail.com>
2018-11-30 14:11:36 -0500

    signin page fix, updated to master

--- website/src/views/Signin/Signin.jsx
+++ website/src/views/Signin/Signin.jsx
@@ -17,39 +17,32 @@ export default class Signin extends PureComponent {
       email: '',
       password: '',
       loading: false,
-      visible: false,
+      showModal: false,
     };
     this.signin = this.signin.bind(this);
-    this.showModal = this.showModal.bind(this);
     this.forgotPassword = this.forgotPassword.bind(this);
+    this.handleOk = this.handleOk.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
   }
 
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
+  /* Email validation */
+  validateEmail(email) {
+    const filter = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (filter.test(email) && email !== '') return true;
+    return false;
   }
 
-  forgotPassword = (e) => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  }
-
-  handleCancel = (e) => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
+  /* Password validation */
+  validatePassword(password) {
+    if (password !== '') return true;
+    return false;
   }
 
   signin() {
     const { email, password } = this.state;
     const { location, history } = this.props;
-    if (email === '' || password === '') {
-      message.error('Looks like you\'re missing something.');
+    if (!this.validateEmail(email) || !this.validatePassword(password)) {
+      message.error('Please enter valid eamil and password');
       return;
     }
     firebase.auth().signInWithEmailAndPassword(email, password)
@@ -67,10 +60,41 @@ export default class Signin extends PureComponent {
       });
   }
 
+  /* Pop out a forgot password window */
+  forgotPassword = () => {
+    this.setState({
+      showModal: true,
+    });
+  }
+
+  /* Handle cancel button in the forgot password window */
+  handleCancel = () => {
+    this.setState({
+      showModal: false,
+    });
+  }
+
+  /* Handle ok button in the forgot password window, send user a password reset email using firebase function */
+  handleOk() {
+    const { email } = this.state;
+    if (!this.validateEmail(email)) {
+      message.error('Please enter valid eamil');
+      return;
+    }
+    firebase.auth().sendPasswordResetEmail(email).then(() => {
+      message.success('Password reset email has been sent.')
+    }).catch((err) => {
+      message.error(err.message);
+    });
+    this.setState({
+      showModal: false,
+    });
+  }
+
   render() {
     const {
-      email, password, loading, visible,
-    } = this.state;
+ email, password, loading, showModal 
+} = this.state;
     const { history } = this.props;
     const user = firebase.auth().currentUser;
     if (user) {
@@ -111,17 +135,18 @@ export default class Signin extends PureComponent {
             <Button size="large" block style={{ marginTop: 10 }} href="/signup">
               <p style={{ margin: 0, display: 'inline' }}>Don&#39;t have an account? Sign Up.</p>
             </Button>
-            <Button size="large" block style={{ marginTop: 10 }} onClick={this.showModal}>
+            <Button size="large" block style={{ marginTop: 10 }} onClick={this.forgotPassword}>
               <p style={{ margin: 0, display: 'inline' }}>Forgot Password?</p>
             </Button>
             <Modal
-              title="Forgot password"
-              className="forgot-password-modal"
-              visible={visible}
               onOk={this.handleOk}
               onCancel={this.handleCancel}
+              className="forgot-password-modal"
+              centered
+              visible={showModal}
+              title="Forgot Password"
             >
-              <p className="forgot-password-label">Please enter your E-mail address:</p>
+              <p className="forgot-password-label">Please enter your E-mail address, we will send you a password reset email:</p>
               <Input
                 required
                 size="large"
