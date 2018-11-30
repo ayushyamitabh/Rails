875fcbc315263acdaf1ad8c77d51e772fde957fd
edwin0108 <qmpyv2000@hotmail.com>
2018-11-30 14:08:24 -0500

    added test case for join class

--- website/src/views/JoinClass/JoinClass.test.jsx
+++ website/src/views/JoinClass/JoinClass.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {
   configure,
+  shallow,
 } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { JoinClass } from '..';
@@ -9,8 +10,26 @@ import { JoinClass } from '..';
 configure({ adapter: new Adapter() });
 
 
+/* Snapshot */
 describe('JoinClass', () => {
   it('Render JoinClass without any eror', () => {
     expect(renderer.create(<JoinClass />).toJSON()).toMatchSnapshot();
+  });
+});
+
+/* Enzyme */
+const wrapper = shallow(<JoinClass />);
+describe('JoinClass shallow', () => {
+  it('Join-class-label name should not be change', () => {
+    expect(wrapper.find('.join-class-label').every('.join-class-label')).toBe(true);
+  });
+  it('Title tag should exists', () => {
+    expect(wrapper.find('.title').closest('.join-class-page').length).toBe(1);
+  });
+  it('Join-card should exists', () => {
+    expect(wrapper.exists('.join-card')).toBe(true);
+  });
+  it('Join-class-table should within the join-card', () => {
+    expect(wrapper.find('.join-class-table').parent().is('.join-card')).toBe(true);
   });
 });
--- website/src/views/Signin/Signin.jsx
+++ website/src/views/Signin/Signin.jsx
@@ -17,32 +17,39 @@ export default class Signin extends PureComponent {
       email: '',
       password: '',
       loading: false,
-      showModal: false,
+      visible: false,
     };
     this.signin = this.signin.bind(this);
+    this.showModal = this.showModal.bind(this);
     this.forgotPassword = this.forgotPassword.bind(this);
-    this.handleOk = this.handleOk.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
   }
 
-  /* Email validation */
-  validateEmail (email) {
-    const filter = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (filter.test(email) && email !== '') return true;
-      return false;
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
   }
 
-  /* Password validation */
-  validatePassword (password) {
-    if (password !== '' ) return true;
-      return false;
+  forgotPassword = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
   }
 
   signin() {
     const { email, password } = this.state;
     const { location, history } = this.props;
-    if ( !this.validateEmail(email) || !this.validatePassword(password) ) {
-      message.error('Please enter valid eamil and password');
+    if (email === '' || password === '') {
+      message.error('Looks like you\'re missing something.');
       return;
     }
     firebase.auth().signInWithEmailAndPassword(email, password)
@@ -60,39 +67,10 @@ export default class Signin extends PureComponent {
       });
   }
 
-/* Pop out a forgot password window */
-  forgotPassword = () => {
-    this.setState({
-      showModal: true,
-    });
-  }
-
-/* Handle cancel button in the forgot password window */
-  handleCancel = () => {
-    this.setState({
-      showModal: false,
-    });
-  }
-
-/* Handle ok button in the forgot password window, send user a password reset email using firebase function */
-  handleOk() {
-    const { email } =  this.state;
-    if ( !this.validateEmail(email) ) {
-      message.error('Please enter valid eamil');
-      return;
-    }
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
-      message.success('Password reset email has been sent.')
-    }).catch(function(err) {
-      message.error(err.message);
-    });
-    this.setState({
-      showModal: false,
-    });
-}
- 
   render() {
-    const { email, password, loading, showModal } = this.state;
+    const {
+      email, password, loading, visible,
+    } = this.state;
     const { history } = this.props;
     const user = firebase.auth().currentUser;
     if (user) {
@@ -133,18 +111,17 @@ export default class Signin extends PureComponent {
             <Button size="large" block style={{ marginTop: 10 }} href="/signup">
               <p style={{ margin: 0, display: 'inline' }}>Don&#39;t have an account? Sign Up.</p>
             </Button>
-            <Button size="large" block style={{ marginTop: 10 }} onClick={this.forgotPassword}>
+            <Button size="large" block style={{ marginTop: 10 }} onClick={this.showModal}>
               <p style={{ margin: 0, display: 'inline' }}>Forgot Password?</p>
             </Button>
             <Modal
+              title="Forgot password"
+              className="forgot-password-modal"
+              visible={visible}
               onOk={this.handleOk}
               onCancel={this.handleCancel}
-              className="forgot-password-modal"
-              centered
-              visible={showModal}
-              title="Forgot Password"
             >
-              <p className="forgot-password-label">Please enter your E-mail address, we will send you a password reset email:</p>
+              <p className="forgot-password-label">Please enter your E-mail address:</p>
               <Input
                 required
                 size="large"
