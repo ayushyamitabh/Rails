4c83aa84211ff88caeae8f936bcad25dbaab45f4
whuang001 <whuang0079@gmail.com>
2018-11-29 23:46:34 -0500

    add enzyme test for NotFound page

--- website/src/views/NotFound/NotFound.test.jsx
+++ website/src/views/NotFound/NotFound.test.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {
+  configure,
+} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { NotFound } from '..';
 import '../../utils/tests/test.css';
+
+configure({ adapter: new Adapter() });
 
 describe('NotFound View', () => {
   it('Expect the NotFound page to match snap shot', () => { // eslint-disable-line no-undef
@@ -10,5 +16,16 @@ describe('NotFound View', () => {
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot(); // eslint-disable-line no-undef
+  });
+
+  /* enzyme testing */
+  const wrapper = shallow(<NotFound />);
+
+  it('should contains a sub Title to say what is going on', () => {
+    expect(wrapper.contains(<h2 className="subTitle">404 Page Not Found</h2>)).toBe(true);
+  });
+
+  it('should contains a button to go back', () => {
+    expect(wrapper.exists('.home-button')).toBe(true);
   });
 });
